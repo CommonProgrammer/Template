d33a14a8a47b5ab26c91b888d3f92a05848695e7
CommonProgrammer <mariusz.kohut@gmail.com>
2017-12-10 00:19:16 +0100

    Improvements

--- authservice/src/main/java/pl/kohutmariusz/authservice/AuthServiceApplication.java
+++ authservice/src/main/java/pl/kohutmariusz/authservice/AuthServiceApplication.java
@@ -1,26 +1,18 @@
 package pl.kohutmariusz.authservice;
 
 
-import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.ServiceInstance;
-import org.springframework.cloud.client.discovery.DiscoveryClient;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 
 @SpringBootApplication
 @EnableAsync
-@EnableDiscoveryClient
+@EnableEurekaClient
 public class AuthServiceApplication {
 
     public static void main(String[] args) throws Exception {
@@ -30,18 +22,5 @@ public class AuthServiceApplication {
     @Bean
     public BCryptPasswordEncoder passwordEncoder(){
        return new BCryptPasswordEncoder();
-    }
-
-    @RestController
-    class ServiceInstanceRestController {
-
-        @Autowired
-        private DiscoveryClient discoveryClient;
-
-        @RequestMapping("/service-instances/{applicationName}")
-        public List<ServiceInstance> serviceInstancesByApplicationName(
-                @PathVariable String applicationName) {
-            return this.discoveryClient.getInstances(applicationName);
-        }
     }
 }
--- configurationservice/src/main/java/pl/kohutmariusz/configurationservice/ConfigurationServiceApplication.java
+++ configurationservice/src/main/java/pl/kohutmariusz/configurationservice/ConfigurationServiceApplication.java
@@ -3,9 +3,11 @@ package pl.kohutmariusz.configurationservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @EnableConfigServer
 @SpringBootApplication
+@EnableEurekaClient
 public class ConfigurationServiceApplication {
 
     public static void main(String[] args) throws Exception {
